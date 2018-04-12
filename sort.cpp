ef71f7c3a48b8a4e00081975c788158e21f62dfd
adzkarfauzie <adzkarfauzie02@gmail.com>
2018-04-12 08:54:27 +0700

    Adding Selection Sort

--- sort.cpp
+++ sort.cpp
@@ -1,13 +1,38 @@
 #include "sort.h"
 
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+ 
+void selectionSort(int arr[], int n)
+{
+    int i, j, min_idx;
+ 
+    // One by one move boundary of unsorted subarray
+    for (i = 0; i < n-1; i++)
+    {
+        // Find the minimum element in unsorted array
+        min_idx = i;
+        for (j = i+1; j < n; j++)
+          if (arr[j] < arr[min_idx])
+            min_idx = j;
+ 
+        // Swap the found minimum element with the first element
+        swap(&arr[min_idx], &arr[i]);
+    }
+}
+
 
 
 void sortingManager(list_stud &list_manager) {
     int n = banyakStud(list_manager);
 
     if(n > 0) {
-        vector<int>manager (n);
-
+        // vector<int>manager (n);
+        int manager[n];
         // Menasignment Nilai kepada Vector
         adr_stud P = list_manager.first;
         int i = 0;
@@ -18,7 +43,8 @@ void sortingManager(list_stud &list_manager) {
             P = P->next;
         }
         // melakukan sorting
-        sort(manager.begin(), manager.end());
+        // sort(manager.begin(), manager.end());
+        selectionSort(manager,n);
         // output data
         P = list_manager.first;
         i = 0;
--- sort.h
+++ sort.h
@@ -3,4 +3,6 @@
 
 #include "head.h"
 
+void swap(int *xp, int *yp);
+void selectionSort(int arr[], int n);
 void sortingManager(list_stud &list_manager);
